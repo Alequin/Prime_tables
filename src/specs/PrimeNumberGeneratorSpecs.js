2e069e536a232ad5e6a8ec7606db76ac250f8845
James Cox <j-cox03@hotmail.com>
2017-12-06 14:33:30 +0000

    Made test for condition: can generate two prime numbers

--- src/specs/PrimeNumberGeneratorSpecs.js
+++ src/specs/PrimeNumberGeneratorSpecs.js
@@ -3,15 +3,22 @@ import { generatePrimeNumbers } from "./../PrimeNumberGenerator.js"
 
 describe("Prime number generator", function(){
 
+  it("Can generate zero prime numbers", () => {
+    let expected = []
+    let result = generatePrimeNumbers(0)
+    assert.deepEqual(result, expected)
+  })
+
   it("Can generate one prime number", () => {
     let expected = [2]
     let result = generatePrimeNumbers(1)
     assert.deepEqual(result, expected)
   })
 
-  it("Can generate zero prime numbers", () => {
-    let expected = []
-    let result = generatePrimeNumbers(0)
+  it("Can generate two prime number", () => {
+    let expected = [2, 3]
+    let result = generatePrimeNumbers(2)
     assert.deepEqual(result, expected)
   })
+
 })
