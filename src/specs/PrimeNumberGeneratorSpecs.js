10efb95695d11ae97899f8069999737763c0d514
James Cox <j-cox03@hotmail.com>
2017-12-06 15:02:41 +0000

    Made tests for generating large prime numbers

--- src/specs/PrimeNumberGeneratorSpecs.js
+++ src/specs/PrimeNumberGeneratorSpecs.js
@@ -15,28 +15,52 @@ describe("Prime number generator", function(){
     assert.deepEqual(result, expected)
   })
 
-  it("Can generate two prime number", () => {
+  it("Can generate two prime numbers", () => {
     let expected = [2, 3]
     let result = generatePrimeNumbers(2)
     assert.deepEqual(result, expected)
   })
 
-  it("Can generate four prime number", () => {
+  it("Can generate four prime numbers", () => {
     let expected = [2, 3, 5, 7]
     let result = generatePrimeNumbers(4)
     assert.deepEqual(result, expected)
   })
 
-  it("Can generate ten prime number", () => {
+  it("Can generate ten prime numbers", () => {
     let expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
     let result = generatePrimeNumbers(10)
     assert.deepEqual(result, expected)
   })
 
-  it("Can generate 20 prime number", () => {
+  it("Can generate 20 prime numbers", () => {
     let expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
     let result = generatePrimeNumbers(20)
     assert.deepEqual(result, expected)
+  })
+
+  it("Can generate 1000 prime numbers", () => {
+    const nums = generatePrimeNumbers(1000)
+
+    let expected = 1000
+    let result = nums.length
+    assert.strictEqual(result, expected)
+
+    expected = 7919
+    result = nums[999]
+    assert.strictEqual(result, expected)
+  })
+
+  it("Can generate 100000 prime numbers", () => {
+    const nums = generatePrimeNumbers(100000)
+
+    let expected = 100000
+    let result = nums.length
+    assert.strictEqual(result, expected)
+
+    expected = 1299709
+    result = nums[99999]
+    assert.strictEqual(result, expected)
   })
 
 })
