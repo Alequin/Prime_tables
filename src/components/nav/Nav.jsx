6e979c083ae995576c0d793b85ca3f549681f235
James Cox <j-cox03@hotmail.com>
2017-12-06 19:12:03 +0000

    Made shell of nav bar

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Nav extends React.Component{
+
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <div>
+        Nav Bar
+      </div>
+    )
+  }
+}
+
+export default Nav
