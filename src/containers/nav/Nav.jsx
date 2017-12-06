3cdb96ac90010283806366898799a36baf59e0b4
James Cox <j-cox03@hotmail.com>
2017-12-06 20:05:14 +0000

    Nav: added method to handle input change and prop to define button function

--- src/containers/nav/Nav.jsx
+++ src/containers/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from "react"
+import PropTypes from 'prop-types'
 
 class Nav extends React.Component{
 
   constructor(props){
     super(props)
+    this.onChangeInputBox = this.onChangeInputBox.bind(this)
+    this.state = {
+      inputText: ""
+    }
+  }
+
+  onChangeInputBox(event){
+    this.setState({
+      inputText: event.target.value
+    })
   }
 
   render(){
@@ -12,15 +23,29 @@ class Nav extends React.Component{
         <label>
           <h2>Enter how many prime numbers you wish to generate</h2>
           <div className="align-center-container">
-            <input className="input" type="number"/>
+            <input
+              className="input"
+              type="number"
+              value={this.state.inputText}
+              onChange={this.onChangeInputBox}
+            />
           </div>
         </label>
         <div className="align-center-container">
-          <input className="button" type="submit" value="Generate prime number multiplication table"/>
+          <input
+            className="button"
+            type="submit"
+            value="Generate prime number multiplication table"
+            onClick={this.props.onClickButton}
+          />
         </div>
       </nav>
     )
   }
 }
+
+Nav.propTypes = {
+  onClickButton: PropTypes.func,
+};
 
 export default Nav
