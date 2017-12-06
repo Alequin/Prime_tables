fd913926c85a00c42e1f104777df308fca461a4c
James Cox <j-cox03@hotmail.com>
2017-12-06 14:33:49 +0000

    passed test for condition: can generate two prime numbers

--- src/PrimeNumberGenerator.js
+++ src/PrimeNumberGenerator.js
@@ -2,4 +2,19 @@
 export const generatePrimeNumbers = (amount) => {
   if(amount === 0) return []
   if(amount === 1) return [2]
+
+  let primeNumbers = [2]
+  let num = 3
+  while(primeNumbers.length < amount){
+    let isCurrentPrime = true;
+    for(let primeNum of primeNumbers){
+      if(num % primeNum === 0){
+        isCurrentPrime = false
+        break
+      }
+    }
+    if(isCurrentPrime) primeNumbers.push(num)
+    num++
+  }
+  return primeNumbers
 }
