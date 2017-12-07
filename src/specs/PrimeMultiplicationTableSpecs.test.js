bd8e382c08b9aafe94f48abf1cd6fcd35529caa2
James Cox <j-cox03@hotmail.com>
2017-12-07 10:25:14 +0000

    PrimeMultiplicationTable: made test for generating prime numbers on construction

--- src/specs/PrimeMultiplicationTableSpecs.test.js
+++ src/specs/PrimeMultiplicationTableSpecs.test.js
@@ -10,7 +10,9 @@ describe("test", function(){
     primeTable = new PrimeMultiplicationTable(5)
   })
 
-  it("can", () => {
-
+  it("generates the required number of prime numbers on construction", () => {
+    let expected = 5
+    let result = primeTable.primeNumbers.length
+    assert.strictEqual(result, expected)
   })
 })
