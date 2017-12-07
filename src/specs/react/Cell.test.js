51811ad7106a07187d50348399bdaf2243e8ce58
James Cox <j-cox03@hotmail.com>
2017-12-07 12:54:18 +0000

    Provided prop in cell component test

--- src/specs/react/Cell.test.js
+++ src/specs/react/Cell.test.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer
-    .create(<Cell/>)
+    .create(<Cell value={5}/>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
