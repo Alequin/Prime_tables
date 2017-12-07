0b3eb8de0383bb9942f45de90c3e7b455e8db4f3
James Cox <j-cox03@hotmail.com>
2017-12-07 16:51:18 +0000

    Made table scrollable with arrow keys

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -8,14 +8,58 @@ class Table extends React.Component{
     super(props)
     this.maxRows = 18
     this.maxColumns = 22
+    this.setEventListeners()
+    this.state = {
+      x: 0,
+      y: 0
+    }
+  }
+
+  setEventListeners(){
+    document.addEventListener("keydown", (event) => {
+      switch(event.key){
+        case "ArrowUp":
+          this.onPressUp()
+        break;
+        case "ArrowDown":
+          this.onPressDown()
+        break;
+        case "ArrowLeft":
+          this.onPressLeft()
+        break;
+        case "ArrowRight":
+          this.onPressRight()
+        break;
+      }
+    })
+  }
+
+  onPressUp(){
+    if(this.state.x > 0){
+      this.setState({x: this.state.x-1})
+    }
+  }
+
+  onPressDown(){
+    this.setState({x: this.state.x+1})
+  }
+
+  onPressLeft(){
+    if(this.state.y > 0){
+      this.setState({y: this.state.y-1})
+    }
+  }
+
+  onPressRight(){
+    this.setState({y: this.state.y+1})
   }
 
   buildRow(tableBuilder, rowIndex){
     const row = []
-    let length = this.props.tableBuilder.maxPosition
-    if(length > this.maxColumns) length = this.maxColumns
+    let length = tableBuilder.maxPosition+this.state.y
+    if(length > this.maxColumns) length = this.maxColumns+this.state.y
 
-    for(let column=0; column<length; column++){
+    for(let column=this.state.y; column<length; column++){
       const cellValue = tableBuilder.valueAt(rowIndex, column)
       const info = tableBuilder.infoAt(rowIndex, column)
       row.push(<Cell key={cellValue} value={cellValue} info={info}/>)
@@ -25,9 +69,9 @@ class Table extends React.Component{
 
   buildTable(){
     const table = []
-    let length = this.props.tableBuilder.maxPosition
-    if(length > this.maxRows) length = this.maxRows
-    for(let row=0; row<length; row++){
+    let length = this.props.tableBuilder.maxPosition+this.state.x
+    if(length > this.maxRows) length = this.maxRows+this.state.x
+    for(let row=this.state.x; row<length; row++){
       table.push(this.buildRow(this.props.tableBuilder, row))
     }
     return table
