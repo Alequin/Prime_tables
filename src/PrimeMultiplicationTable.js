9ac01a852d7f9396fe115a03f6b21a837ed9fdfe
James Cox <j-cox03@hotmail.com>
2017-12-07 10:23:23 +0000

    Made shell of PrimeMultiplicationTable class

---
+++ src/PrimeMultiplicationTable.js
@@ -0,0 +1,8 @@
+
+class PrimeMultiplicationTable{
+  constructor(){
+
+  }
+}
+
+export default PrimeMultiplicationTable
---
+++ src/specs/PrimeMultiplicationTableSpecs.test.js
@@ -0,0 +1,16 @@
+
+import assert from "assert"
+import PrimeMultiplicationTable from "./../PrimeMultiplicationTable"
+
+describe("test", function(){
+
+  let primeTable
+
+  beforeEach(() => {
+    primeTable = new PrimeMultiplicationTable(5)
+  })
+
+  it("can", () => {
+
+  })
+})
