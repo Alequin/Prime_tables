0ffc67755024339cb7eb0b19166700809b408ef6
James Cox <j-cox03@hotmail.com>
2017-12-07 10:44:31 +0000

    PrimeMultiplicationTable: passed test for accessing 0,0

--- src/PrimeMultiplicationTable.js
+++ src/PrimeMultiplicationTable.js
@@ -6,8 +6,9 @@ class PrimeMultiplicationTable{
   }
 
   valueAt(x, y){
-    if(x == 0) return this.primeNumbers[y-1]
-    if(y == 0) return this.primeNumbers[x-1]
+    if(x === 0 && y === 0) return " "
+    if(x === 0) return this.primeNumbers[y-1]
+    if(y === 0) return this.primeNumbers[x-1]
     return this.primeNumbers[x-1] * this.primeNumbers[y-1]
   }
 }
